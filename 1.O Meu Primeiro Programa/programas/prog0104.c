a49645b763b3626d05f8c5167056ca258c014be7
Moises Nunes <moisesnunes10086@gmail.com>
2025-08-17 15:54:25 -0300

    Capitulo 1

---
+++ 1.O Meu Primeiro Programa/programas/prog0104.c
@@ -0,0 +1,22 @@
+// Problema: Escreva um programa em C que tenha a seguinte saída:
+/*
+C
+is the greatest
+Language
+*/
+
+#include <stdio.h>
+main()
+{
+    printf("C\n");
+    printf("is the greatest\n");
+    printf("Language\n");
+}
+
+// ou
+
+#include <stdio.h>
+main()
+{
+    printf("C\nis the greatest\nLanguage\n");
+}
---
+++ 1.O Meu Primeiro Programa/programas/prog0105.c
@@ -0,0 +1,12 @@
+// (Com erro de compilção)
+#include <stdio.h>
+main()
+{
+    printf("Hoje está um " LINDO " dia!!!\n");
+}
+
+#include <stdio.h>
+main()
+{
+    printf("Hoje está um \"LINDO\" dia!!!\n");
+}
